73c253d904edf74776bcb6bf0a66cc617d522159
Nathanael Blocher <blocher.nathanael@gmail.com>
2024-01-10 08:54:20 -0800

    added petshop.delete()

--- 1-fs/fs.js
+++ 1-fs/fs.js
@@ -17,7 +17,7 @@ const petshop = {
             }
             // if the index is defined, parse it
             else {
-                let i = +index;
+                let i = parseInt(index);
                 // if it's an out-of-bounds index
                 if (!pets[i]) {
                     // output an error
@@ -55,7 +55,7 @@ const petshop = {
             pets = JSON.stringify(pets);
 
             // attempt to write to the pets.json file
-            fs.writeFile("../pets.json", pets, "utf-8", (error, text) => {
+            fs.writeFile("../pets.json", pets, "utf-8", (error) => {
                 // if there is an error writing, display it
                 if (error) { throw error };
                 // otherwise, log the new pet that we just created
@@ -63,29 +63,66 @@ const petshop = {
                 console.log(newPet); process.exit();
             });
         });
+    },
+    delete: (index) => {
+        // if there's no index, display usage
+        if (index === undefined) {
+            console.error("Usage: node fs.js destroy int(INDEX)");
+            process.exitCode = 1; process.exit();
+        }
+        // parse the index if it exists
+        let i = parseInt(index);
+
+        // attempt to read the pets.json file
+        fs.readFile("../pets.json", "utf-8", (error, text) => {
+            // if there's an error reading the file, display it
+            if (error) { throw error; }
+
+            // parse the text into an array using JSON.parse()
+            let pets = JSON.parse(text);
+
+            // if there isn't an item at the given index, display usage
+            if (!pets[i]) {
+                console.error(`Did not find pet at index ${i}`);
+                console.error("Usage: node fs.js destroy int(INDEX)");
+                process.exitCode = 1; process.exit();
+            }
+            // otherwise, delete the item at index i using .splice(i, 1)
+            else {
+                let deleted = pets.splice(i, 1);
+                // stringify the pets array
+                pets = JSON.stringify(pets);
+
+                // attempt to write to file
+                fs.writeFile("../pets.json", pets, "utf-8", (error) => {
+                    // if there's an error writing to file, display it
+                    if (error) { throw error; }
+                    // otherwise, log the removed pet
+                    else {
+                        console.log(`Removed the pet at index ${i}`);
+                        console.log(deleted); process.exit();
+                    }
+                });
+            }
+        });
     }
 }
 
 // this switch case executes different behavior based on the third argument value
 switch (process.argv[2]) {
-    case "read": {
+    case "read":
         petshop.read(process.argv[3]);
         break;
-    }
-    case "create": {
+    case "create":
         petshop.create(process.argv[3], process.argv[4], process.argv[5]);
         break;
-    }
-    case "update": {
-        // code that updates a pet
+    case "update":
+        // petshop.update() (not yet created)
         break;
-    }
-    case "destroy": {
-        // code that deletes a pet
+    case "destroy":
+        petshop.delete(process.argv[3]);
         break;
-    }
-    default: {
+    default:
         console.error("Usage: node fs.js [read | create | update | destroy]");
         process.exitCode = 1; process.exit();
-    }
 }
